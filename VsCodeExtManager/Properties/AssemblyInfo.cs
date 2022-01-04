17d4e5d17db570c445caf3949fd9002ea1254de4
Chad Roesler <ChadRoesler@Me.com>
2022-01-04 00:06:31 -0600

    updates for installer

--- VsCodeExtManager/Properties/AssemblyInfo.cs
+++ VsCodeExtManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VsCodeExtManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple way to manage VsCodeExtensions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chad Roesler")]
 [assembly: AssemblyProduct("VsCodeExtManager")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright Chad Roesler ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
